cd8a5800309da6a73598613ad4ea748e0bf09525
DoughyJoey <32750162+DoughyJoey@users.noreply.github.com>
2018-10-03 20:28:27 +0100

    Completing the sign up / login process
    Created MySQL users table
    Created config.php file to connect to databases using php
    Added INSERT functionality to store user details in database
    Added username validation and login handling functionality
    Created session variables

---
+++ Learnify/includes/classes/Constants.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ */
+class Constants {
+
+	public static $passwordsDoNotMatch = "Your passwords don't match";
+	public static $passwordsNotAlphanumeric = "Your password can only contain numbers and letters";
+	public static $passwordsCharacters = "Your password must be between 5 and 25 characters";
+	public static $emailsInvalid = "Your Email is invalid";
+	public static $emailsDoNotMatch = "Your Email Addresses don't match";
+	public static $emailTaken = "This email is already in use";
+	public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
+	public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
+	public static $usernameCharacters = "Your username must be between 5 and 25 characters";
+	public static $usernameTaken = "This username already exists";
+
+	public static $loginFailed = "Your username or password was incorrect";
+
+}
+
+
+
+?>
