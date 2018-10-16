dd83b5ef8a9485e6ee8cda680e1393dcad7ad123
tony <4reals@ys-BookAir.local>
2018-10-16 22:37:30 +0100

    adding more content to UI, icons and updating styling

--- Learnify/index.php
+++ Learnify/index.php
@@ -16,10 +16,48 @@ if(isset($_SESSION['userLoggedIn'])){
 <head>
 	<title>Welcome to Learnify!</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  
+
+  
 </head>
 <body>
-
-	<div id="nowPlayingBarContainer">
+  
+  <div id="mainContianer">
+    <div id="topContainer">
+      
+      <div id="navBarContainer">
+        <nav class="navBar">
+          <a href="index.php" class="logo">
+            <img src="assets/images/icons/logo.png">
+          </a>
+          
+          <div class="group">
+            <div class="navItem">
+              <a href="search.php" class="navItemLink"> Search 
+                <img src="assets/images/icons/search.png" class="icon" alt="Search">
+              </a>
+            </div>
+          </div>
+          
+          <div class="group">
+            <div class="navItem">
+              <a href="rowse.php" class="navItemLink"> Browse </a>
+            </div>
+            <div class="navItem">
+              <a href="yourMusic.php" class="navItemLink"> Your Music </a>
+            </div>
+            <div class="navItem">
+              <a href="profile.php" class="navItemLink"> Profile </a>
+            </div>
+            
+          </div>
+        
+        </nav>
+      </div>
+      
+    </div>
+    
+    <div id="nowPlayingBarContainer">
 		<div id="nowPlayingBar">
 			<div id=nowPlayingLeft>
 
@@ -113,6 +151,12 @@ if(isset($_SESSION['userLoggedIn'])){
 		</div>
 		
 	</div>
+    
+  </div>
+  
+  
+
+	
 
 </body>
 </html>
