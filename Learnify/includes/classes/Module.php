365b29c433643e9c62589f834ad8242655eea10a
tonyennis91 <tonyennis91@gmail.com>
2018-11-12 17:30:25 -0500

    adding Module class

---
+++ Learnify/includes/classes/Module.php
@@ -0,0 +1,46 @@
+<?php
+	class Module {
+
+		private $con;
+		private $id;
+    
+    private $title;
+    private $course;
+    private $lecturer;
+    private $artworkPath;
+		
+		public function __construct($con, $id) {
+			$this->con = $con;
+			$this->id = $id;
+      
+      $query = mysqli_query($this->con, "SELECT * FROM modules WHERE id='$this->id'");
+      $module = mysqli_fetch_array($query);
+      
+      $this->title = $module['moduleTitle'];
+      $this->course = $module['course'];
+      $this->lecturer = $module['lecturer'];
+      $this->artworkPath = $module['artworkPath'];
+      
+		}
+    
+    // function to retrieve the lecturer's name
+    // pass ID into the sql Query to artists table.
+    public function getTitle() {
+      
+      return $this->title;
+    }
+    
+    public function getLecturer() {
+      return new Lecturer($this->con, $this->lecturer);
+    }
+    
+    public function getArtworkPath() {
+      return $this->artowrkPath;
+    }
+    
+    public function getCourse() {
+      return $this->course;
+    }
+    
+  }
+?>
