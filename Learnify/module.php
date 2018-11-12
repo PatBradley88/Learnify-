30f4f2a8b4f3c24c68e1196e1410e4741a371b30
tonyennis91 <tonyennis91@gmail.com>
2018-11-12 17:31:52 -0500

    adding module view

---
+++ Learnify/module.php
@@ -0,0 +1,38 @@
+<?php include("includes/header.php"); 
+  
+  if(isset($_GET['id'])) {
+    $moduleID = $_GET['id'];
+  }
+  else {
+    header("Location: index.php");
+  }
+
+// you will need to rename the Modules table to be 'modules'
+// $moduleQuery = mysqli_query($con, "SELECT * FROM modules WHERE id='$moduleID'");
+// $module = mysqli_fetch_array($moduleQuery);
+
+// get the ID of the lecturer for the module in the modules table. will return an int.
+// $lecturerId = $module['lecturer'];
+
+
+$module = new Module($con, $moduleID);
+$lecturer = $module->getLecturer();
+
+// new Lecturer($con, $lecturerId);
+
+
+
+
+
+
+
+
+
+echo $module->getTitle(). "<br>";
+//get name of lecturer for the module
+echo $lecturer->getName();
+?>
+
+
+
+<?php include("includes/footer.php"); ?>
