0673dbc5a4d776c3a18b1408821aee7f40da7392
tens91 <tonyennis91@gmail.com>
2018-11-12 18:26:43 -0500

    fucking spaces

--- Learnify/module.php
+++ Learnify/module.php
@@ -14,25 +14,25 @@
 // get the ID of the lecturer for the module in the modules table. will return an int.
 // $lecturerId = $module['lecturer'];
 
-
 $module = new Module($con, $moduleID);
 $lecturer = $module->getLecturer();
 
 // new Lecturer($con, $lecturerId);
 
-
-
-
-
-
-
-
-
-echo $module->getTitle(). "<br>";
-//get name of lecturer for the module
-echo $lecturer->getName();
+// echo $module->getTitle(). "<br>";
+// //get name of lecturer for the module
+// echo $lecturer->getName();
 ?>
 
+<div class="entityInfo">
+   <div class="leftSection">
+     <img src="./<?php echo $module->getArtworkPath(); ?>">
+     
+  </div>
+  <div class="rightSection">
+    
+  </div>
+</div>
 
 
 <?php include("includes/footer.php"); ?>
