fe2358f4cefa79664bd96e48574017b9e4a0bb5b
DoughyJoey <32750162+DoughyJoey@users.noreply.github.com>
2018-11-26 16:38:36 +0000

    Created lecturer page

---
+++ Learnify/lecturer.php
@@ -0,0 +1,34 @@
+<?php
+
+include("includes/includedFiles.php");
+
+//ID passed through the url
+if(isset($_GET['id'])) {
+  $lecturerId = $_GET['id'];
+}
+else {
+  header("Location: index.php");
+}
+
+//creates lecturer object from Lecturer class
+$lecturer = new Lecturer($con, $lecturerId);
+?>
+
+<div class="entityInfo">
+
+	<div class="centerSection">
+
+		<div class="lecturerInfo">
+		
+			<!-- prints lecturer name using lecturer object -->
+			<h1 class="lecturerName"><?php echo $lecturer->getName() ?></h1>
+
+			<div class="headerButtons">
+				<button class="button">PLAY</button>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
