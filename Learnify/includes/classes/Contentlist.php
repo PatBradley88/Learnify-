d63e4bb2ca334bea26cb050cbee085531a09fdec
tony ennis <tonyennis91@gmail.com>
2018-12-09 18:02:50 -0500

    updating playlist content

--- Learnify/includes/classes/Contentlist.php
+++ Learnify/includes/classes/Contentlist.php
@@ -7,6 +7,13 @@
 		private $owner;
 
 		public function __construct($con, $data) {
+      
+      if(!is_array($data)) {
+        //if data is an id (string)
+        $query = mysqli_query($con, "SELECT * FROM contentlists WHERE id='$data'");
+        $data = mysqli_fetch_array($query);
+      }
+      
 			$this->con = $con;
 			$this->id = $data['id'];
 			$this->name = $data['name'];
@@ -24,6 +31,11 @@
 		public function getOwner() {
 			return $this->owner;
 		}
+    
+    public function getNumberOfVideos() {
+      $query = mysqli_query($this->con, "Select id FROM contentlists WHERE id='$this->id'");
+      return mysqli_num_rows($query);
+    }
 	}
 
 ?>
--- Learnify/yourClasses.php
+++ Learnify/yourClasses.php
@@ -14,6 +14,7 @@ include("includes/includedFiles.php");
 
 		<?php
 			$username = $userLoggedIn->getUsername();
+      echo "<p>" . $username . "</p>";
 
 		    $contentlistQuery = mysqli_query($con, "SELECT * FROM contentlists WHERE owner='$username'");
 
